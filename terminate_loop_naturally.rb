a11d38f0022b8c9ae1e551e27b12cb90f0b39d39
Christian Pardo <chrispardopersonal@gmail.com>
2021-02-26 23:32:59 +0000

    Automatically backed up by Learn

---
+++ terminate_loop_naturally.rb
@@ -0,0 +1,5 @@
+count = 0 # a bit of data defined outside the loop
+while count < 3 do # A boolean expression using the bit of data
+puts "I am the #{count}, I love to count!" # Work
+count -= 1 # A bit of work that moves the bit of data closer to being false 
+end
